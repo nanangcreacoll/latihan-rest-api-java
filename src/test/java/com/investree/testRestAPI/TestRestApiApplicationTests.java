47db9f2e44db2826c8c056a2687602ceb1e0f4ae
nanangcreacoll <nanang.creacol.26@gmail.com>
2023-06-10 11:18:33 +0700

    first commit

---
+++ src/test/java/com/investree/testRestAPI/TestRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investree.testRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
